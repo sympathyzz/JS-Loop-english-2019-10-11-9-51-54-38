da20d97fe43c654375677389cab69d2caa009c4c
sympathyzz <51390756+sympathyzz@users.noreply.github.com>
2020-06-20 16:50:15 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for(var i=0;i<=20;i++){
+    if(i==0){
+        console.log(i+" is neither odd nor even");
+    }else if(i%2==0){
+        console.log(i+" is even number");
+    }else {
+        console.log(i+" is odd number");
+    }
+}
